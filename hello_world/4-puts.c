c29121b7594a1744913fee7c931f98d7c65be20e
Denisse Landau <denisselandau97@gmail.com>
2022-06-06 09:38:11 -0700

    fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - will print a string
  *
- * Return: Always 0 (Success)     
+ * Return: Always 0 (Success)
  */
-
-int main (void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 }
