64c1719652cac94128ce8c0daf7bb11960d70864
Denisse Landau <denisselandau97@gmail.com>
2022-06-06 09:25:17 -0700

    first string

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+int main (void) 
+{
+	/*will print a string*/
+
+	char a1[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(a1); /*writing the string to stdout*/
+
+	return(0);
+}
