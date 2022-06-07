0ef9624f17b3c9548a6eb31bf6be7acb58220813
Denisse Landau <denisselandau97@gmail.com>
2022-06-07 06:56:20 -0700

    $1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - will print a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = "a";
+
+	while (x <= "z")
+	{
+		putchar(x);
+		x++;
+	}
+
+}
