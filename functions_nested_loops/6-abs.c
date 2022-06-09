eeba69cefbdaea684daa12634bc7e23374451505
Denisse Landau <denisselandau97@gmail.com>
2022-06-09 11:33:25 -0700

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an integer.
+ * @i: number
+ * Return: absolute value.
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	if (i < 0)
+	{
+		i = -i;
+		return (i);
+	}
+}
