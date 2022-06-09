6d590693a59d49c5de573d4ab5ff8c8fc001cd6e
Denisse Landau <denisselandau97@gmail.com>
2022-06-09 10:15:11 -0700

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -7,7 +7,11 @@
 int _islower(int c)
 {
 	if ((c >= 97) || (c <= 122))
-		return (1);
-	else if ((c >= 65) || (c <= 90))
+	{
+	return (1);
+	}
+	if ((c >= 65) || (c <= 90))
+	{
 		return (0);
+	}
 }
