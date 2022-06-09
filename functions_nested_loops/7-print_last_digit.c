be1b5b6c85b02a538de067f88f96f687ad380c5c
Denisse Landau <denisselandau97@gmail.com>
2022-06-09 12:06:49 -0700

    last difit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit.
+ * @i: number
+ * Return: returns i as last digit.
+ */
+int print_last_digit(int i)
+{
+	i = (i % 10);
+	return (i);
+}
