49cbe9cee9a60a2d20e5198a5f13dbcc5278ac6b
Denisse Landau <denisselandau97@gmail.com>
2022-06-09 11:08:27 -0700

    lower/greater than

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - print sign depending on result
+ * @n: greater than / equal to 0 / lower than
+ * Return: 1 / 0 / -1 depending on result
+ */
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n == '0')
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
