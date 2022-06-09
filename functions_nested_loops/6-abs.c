fc8bbd3a61f045d098e88b9f160439d6cce516fc
Denisse Landau <denisselandau97@gmail.com>
2022-06-09 11:45:22 -0700

    absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
  * _abs - absolute value of an integer.
  * @i: number
- * Return: absolute value.
+ * Return: i absolute value.
  */
 int _abs(int i)
 {
@@ -10,7 +10,7 @@ int _abs(int i)
 		return (i);
 	if (i < 0)
 	{
-		i = ( i * (-1));
+		i = (i * (-1));
 		return (i);
 	}
 }
