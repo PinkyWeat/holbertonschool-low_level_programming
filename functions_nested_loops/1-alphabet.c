ba4eb585bb60760c25854d8b8965a7ec41dde03f
Denisse Landau <denisselandau97@gmail.com>
2022-06-09 06:53:46 -0700

    for _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i < = 'z'; i++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+	return (0);
+}
