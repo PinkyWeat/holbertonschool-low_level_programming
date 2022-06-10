823a5e250338fb7ae37f0e0ef00affb37379cd9e
Denisse Landau <denisselandau97@gmail.com>
2022-06-10 10:08:20 -0700

    fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,12 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 		printf("%d", n);
+		putchar('\n');
 	}
 
 	else if (n < 98)
 	{
-		for (i = 0; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i < 98)
