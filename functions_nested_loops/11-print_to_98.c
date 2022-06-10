9ba5f391e333760ec4395ce65b4fb43f59daa2ff
Denisse Landau <denisselandau97@gmail.com>
2022-06-10 10:04:43 -0700

    fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -19,8 +19,11 @@ void print_to_98(int n)
 		for (i = 0; i <= 98; i++)
 		{
 			printf("%d", i);
-			putchar(',');
-			putchar(' ');
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
@@ -29,8 +32,11 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			putchar(',');
-			putchar(' ');
+			if (i > 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 }
