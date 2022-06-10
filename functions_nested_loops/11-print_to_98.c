6b248b694650c547fa385a91143cefd7f437b1df
Denisse Landau <denisselandau97@gmail.com>
2022-06-10 09:54:10 -0700

    numbers from or up to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 -
+ * @n: num 1.
+ * Return: result.
+ */
+void print_to_98(int n)
+{
+	int i;
+	
+	if (n <= 98)
+	{
+		for (i = 0; i <= 98; i++)
+		{
+			printf("%d", i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	else if ( n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
