672a1017205460b341eae01448f48b30f416b323
Denisse Landau <denisselandau97@gmail.com>
2022-06-10 16:33:58 -0700

    weather report

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more_numbers - shows numbers 0 to 14, 10 times.
+ * 
+ * Return: Always 0.
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + 48);
+			_putchar(10);
+		}
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _putchar(char);
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -11,5 +12,7 @@ int mul(int a, int b);
 void print_numbers(void);
 
 void print_most_numbers(void);
+
+void more_numbers(void);
 
 #endif
