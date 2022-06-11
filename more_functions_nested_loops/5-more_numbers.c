b2d7490acd706b3a6ea3f51194dc936f6a401492
Denisse Landau <denisselandau97@gmail.com>
2022-06-10 17:09:22 -0700

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,13 +15,9 @@ void more_numbers(void)
 		{
 			if (j < 10)
 			{
-				_putchar ((j % 10) + 48);
-			}
-			else if ( j > 10)
-			{
 				_putchar ((j / 10) + 48);
 			}
-
+			_putchar ((j / 10) + 48);
 			
 		}
 		_putchar(10);
