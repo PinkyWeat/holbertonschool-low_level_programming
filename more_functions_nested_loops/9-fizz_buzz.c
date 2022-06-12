4ecb299c5cd423e8f2ecbfa25e2afc37ee511c48
Denisse Landau <denisselandau97@gmail.com>
2022-06-11 17:39:14 -0700

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * fizz_buzz - helps filter 99.5% of candidates.
+ * main - helps filter 99.5% of candidates.
  *
  * Return: 0;
  */
@@ -14,22 +14,22 @@ int main(void)
 		if ((i % 3) && (i % 5))
 		{
 			printf("FizzBuzz");
-			putchar(10);
+			putchar(32);
 		}
 		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
-			putchar(10);
+			putchar(32);
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
-			putchar(10);
+			putchar(32);
 		}
 		else
 		{
 			printf("%d", i);
-			putchar(10);
+			putchar(32);
 		}
 	}
 	return (0);
