85042f03806a0bfe60661780829ec553d95a4231
Denisse Landau <denisselandau97@gmail.com>
2022-06-11 19:13:40 -0700

    largest n

--- debugging/2-largest_number.c
+++ debugging/2-largest_number.c
@@ -24,6 +24,22 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+	else if (a == b && a == c && b == c)
+	{
+		largest = a;
+	}
+	else if (a == b && a > c)
+	{
+		largest = a;
+	}
+	else if (a == c && a > b)
+	{
+		largest = a;
+	}
+	else if (b == c && b > a)
+	{
+		largest = b;
+	}
 
 	return (largest);
 }
