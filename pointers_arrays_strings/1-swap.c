adb5d06ecb8b02c0a15c924b7d452ec24debe296
Denisse Landau <denisselandau97@gmail.com>
2022-06-14 11:32:49 -0700

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* reset_to_98 - resets n to 98
+*
+*  @n: value assigned by user
+*/
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
+
+
+
+			
