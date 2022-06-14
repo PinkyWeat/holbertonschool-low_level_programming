3439ddfb5a9db048f2f1e2a1028898d87e85fba4
Denisse Landau <denisselandau97@gmail.com>
2022-06-14 11:57:39 -0700

    length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strlen - returns the length of a string.
+*  @s: a string.
+* Return: length.
+*/
+int _strlen(char *s)
+{
+	return (sizeof(*str));
+}
