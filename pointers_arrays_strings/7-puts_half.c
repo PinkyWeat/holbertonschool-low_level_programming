bdabf771be111d027f94598dffd77d40c9fe4fd6
Denisse Landau <denisselandau97@gmail.com>
2022-06-15 15:27:10 -0700

    puts half a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - puts half o the string.
+ * @str: print
+ *
+ */
+void puts_half(char *str)
+{
+	int c = 0, n;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+
+	for (n = (c / 2); n <= c; n++)
+	{
+		_putchar(str[n]);
+	}
+}
