b4254325ec366a1f2462894ea7c58338bd159b08
Denisse Landau <denisselandau97@gmail.com>
2022-06-15 11:44:01 -0700

    prints string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts - returns the length of a string.
- * @s: a string.
+ * _puts - print string.
+ * @str: a string.
  *
  */
 void _puts(char *str)
 {
-        int c;
+	int c;
 
-        for (c = 0; str[c] ; c++)
-        {
-               _putchar(str[c]);
-        }
-
+	for (c = 0; str[c] ; c++)
+	{
+		_putchar(str[c]);
+	}
 	_putchar(10);
 }
