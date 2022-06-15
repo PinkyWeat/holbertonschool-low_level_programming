958a9f61f40c1e8ac71367ef7910b12625d4dbfd
Denisse Landau <denisselandau97@gmail.com>
2022-06-15 13:50:13 -0700

    every other char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - print string in reverse.
+ * @s: a string.
+ *
+ */
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		if (c % 2 == 0)
+		{
+			_putchar(str[c]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
