1bc065aa4bb1de6cabcd06100626735f507ecfd5
Denisse Landau <denisselandau97@gmail.com>
2022-06-16 11:10:38 -0700

    dest+src

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcat - appends the src string to the dest string
+ * @dest: string is appended to this string.
+ * @src: appended string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	for (x = 0; dest[x]; x++)
+	;
+
+	for (y = 0; src[y]; y++)
+	{
+		dest[x] = src[y];
+	}
+	return (dest);
+}
