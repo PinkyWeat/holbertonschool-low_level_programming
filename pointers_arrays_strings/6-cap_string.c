a659c07ecf3d9f1011fd7caf16d6ea257164d35f
Denisse Landau <denisselandau97@gmail.com>
2022-06-17 19:41:31 -0700

    upper each word

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string to capitalize.
+ * Return: s.
+ */
+char *cap_string(char *s)
+{
+	int cont;
+
+	for (cont = 0; s[cont]; cont++)
+	{
+		if (cont == 0 && s[cont] != '\0' && s[cont] >= 97 && s[cont] <= 122)
+		{
+			s[0] -= 32;
+		}
+		else if (s[cont] >= 97 && s[cont] <= 122)
+		{
+			if (s[cont - 1] == ',' || s[cont - 1] == ';' ||
+				s[cont - 1] == '.' || s[cont - 1] == '!' ||
+				s[cont - 1] == '?' || s[cont - 1] == '"' ||
+				s[cont - 1] == '(' || s[cont - 1] == ')' ||
+				s[cont - 1] == '{' || s[cont - 1] == '}' ||
+				s[cont - 1] == ' ' || s[cont - 1] == '\t' ||
+				s[cont - 1] == '\n')
+			{
+				s[cont] -= 32;
+			}
+		}
+	}
+	return (s);
+}
