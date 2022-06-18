858919aad6f25abf1b54ef8dfc88963da0393741
Denisse Landau <denisselandau97@gmail.com>
2022-06-17 19:02:22 -0700

    to upper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,13 @@
 char *string_toupper(char *toUpper)
 {
 	int i;
-	
+
 	for  (i = 0; toUpper[i] != 0; i++)
 	{
-		if(toUpper[i] >= 97 && toUpper[i] <= 122)
+		if (toUpper[i] >= 97 && toUpper[i] <= 122)
 		{
 			toUpper[i] -= 32;
 		}
 	}
-	return(toUpper);
+	return (toUpper);
 }
