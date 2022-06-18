9418c73e03109c38f1a66c5d389d58c92fae37f3
Denisse Landau <denisselandau97@gmail.com>
2022-06-18 08:08:47 -0700

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * *_memset - fills the first n bytes of memory area pointed to by s w/cnst byte b.
+ * *_memset - fills first n bytes of memarea pointed by s w/cnst b.
  * @n: num of bytes of memory area pointed to by 2
+ * @s: string.
+ * @b: cnst byte.
  * Return: pointer to the memory to print.
  */
 char *_memset(char *s, char b, unsigned int n)
