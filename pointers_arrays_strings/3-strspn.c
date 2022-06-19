73ffbee80b2c780daa64c0e1fc30ee4fa8278290
Denisse Landau <denisselandau97@gmail.com>
2022-06-19 10:42:05 -0700

    for get length

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,17 +8,17 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, x;
-	unsigned int rbytes = 0;
+	unsigned int rbytes = 0; /* important to start it at 0 */
 
 	for (i = 0; i <= 5; i++)
 	{
-		for (x = 0; accept[x]; x++)
+		for (x = 0; accept[x]; x++) /* did it with for 'cause of the x = 0 part. */
 		{
-			if(s[i] == accept[x])
+			if (s[i] == accept[x])
 			{
 				rbytes++;
 			}
 		}
 	}
-	return(rbytes);
+	return (rbytes);
 }
