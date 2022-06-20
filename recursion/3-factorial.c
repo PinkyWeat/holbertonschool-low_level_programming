fd638af173004ee09b9e5194f1e5673566a7b13a
Denisse Landau <denisselandau97@gmail.com>
2022-06-20 08:16:13 -0700

    factorial of

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - prints the factorial of a certain given number.
+ * @n: given number.
+ * Return: if n < 0 --> -1 (error).
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (1);
+	else
+		return (n * factorial (n - 1));	
+}
--- recursion/README.md
+++ recursion/README.md
@@ -2,3 +2,5 @@
 
 0-puts-recursion 	- puts recursively chars of an s string.
 1-print-rev-recursion 	- prints a string in reverse.
+2-strlen-recursion	- prints the length of a string.
+3-factorial		- prints factorial of a given number.
