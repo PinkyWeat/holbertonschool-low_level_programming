0433d84b91ea521f96609ab2ef08f7a2e078d72e
Denisse Landau <denisselandau97@gmail.com>
2022-06-21 11:48:57 -0700

    num of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point, prints the num of args passed to it.
+ * @argv: not used here.
+ * @argc: number of args.
+ * Return: 0 for success.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc[c]);
+	return (0);
+}
