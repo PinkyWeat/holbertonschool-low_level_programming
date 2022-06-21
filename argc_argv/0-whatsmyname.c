e83f756d0e04ece4afb55a38e6300ca4b27af072
Denisse Landau <denisselandau97@gmail.com>
2022-06-21 11:36:09 -0700

    argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point, its name, followed by a new line.
+ * @argc: n entered in cmd line
+ * @argv: add string values entered cmd line
+ * Return: 0.
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
