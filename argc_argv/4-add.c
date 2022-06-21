ca8f171739f577f0e6c8ae3ddf36feb59711d801
Denisse Landau <denisselandau97@gmail.com>
2022-06-21 13:11:31 -0700

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,18 +11,18 @@ int main(int argc, char *argv[])
 	int i, j, r;
 
 	for (i = 1; i < argc; i++)
-	;
-	for (j = 0; argv[i][j]; j++)
 	{
-		if (argv[i][j] < 48 || argv[i][j] > 57)
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
 	for (i = 1; i < argc; i++)
-	;
-	r += atoi (argv[i]);
+		r + = atoi (argv[i]);
 	printf("%d\n", r);
 	return (0);
 }
