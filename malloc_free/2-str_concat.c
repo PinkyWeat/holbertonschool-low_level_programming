1b7bced2999055aec24adfb794b691a65ed87895
Denisse Landau <denisselandau97@gmail.com>
2022-06-23 12:53:10 -0700

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,7 +8,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *conc = malloc(strlen(s1 + s2));
+	char *conc = malloc(strlen(s1) + strlen(s2) + 1);
 	int p1, p2; /* p1: paste 1, p2: paste 2 */
 
 	if (s1 == NULL || s2 == null);
