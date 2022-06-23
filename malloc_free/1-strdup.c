bfe495ad3a98792d9997f77c574f053c8a1265cf
Denisse Landau <denisselandau97@gmail.com>
2022-06-23 12:38:13 -0700

    s

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * _strdup - allocate space which contains copy of str.
+ * @str: string we'll copy.
+ * Return: new space in memory.
+ */
 char *_strdup(char *str)
 {
 	int i, len;
@@ -18,5 +22,5 @@ char *_strdup(char *str)
 	for (i = 0; i < len; i++)
 		paste[i] = str[i];
 	paste[i] = '\0';
-	return (paste);	
+	return (paste);
 }
