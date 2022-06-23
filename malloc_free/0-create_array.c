3706f26997077ea49c0707ee0b85b56c6c61e7a5
Denisse Landau <denisselandau97@gmail.com>
2022-06-23 09:37:56 -0700

    malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *first = malloc(size);
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		first[i] = c;
+	}
+	first[i] = '\0';
+	return (first);
+}
