d668e887364f50496aa961c88140c98a169b3148
Denisse Landau <denisselandau97@gmail.com>
2022-06-23 12:54:29 -0700

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-@include <string.h>
+#include <string.h>
 /**
  * _strdup - function that concatenates two strings..
  * @s1: ctrl + c.
@@ -9,7 +9,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *conc = malloc(strlen(s1) + strlen(s2) + 1);
+	char *conc = malloc(strlen(s1 + s2));
 	int p1, p2; /* p1: paste 1, p2: paste 2 */
 
 	if (s1 == NULL || s2 == null);
