4957addd0a7e9dd70546ea1b7e5c0cd456bd3d19
Denisse Landau <denisselandau97@gmail.com>
2022-06-24 12:57:16 -0700

    malloc1

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -9,8 +9,12 @@ void *malloc_checked(unsigned int b)
 {
 	int *array;
 
-	array = malloc(b * sizeof(unsigned int *));
+	array = malloc(b);
 	if (array == NULL)
-		return ("98");
+	{
+		free(array);
+		exit(98);
+	}
+	
 	return (array);
 }
