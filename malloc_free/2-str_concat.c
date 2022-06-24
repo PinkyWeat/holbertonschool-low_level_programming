8917d2a7e790b45beeccede8fe95f74c29ac5215
Denisse Landau <denisselandau97@gmail.com>
2022-06-24 06:39:32 -0700

    cases fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,12 +14,18 @@ char *str_concat(char *s1, char *s2)
 
 	len = (strlen(s1) + strlen(s2) + 1);
 	conc = malloc(len);
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
+	
+	if (s1 != NULL && s2 == NULL)
+		len = strlen(s1);
+	if (s1 == NULL && s2 != NULL)
+		len = strlen(s2);
+	if (s1 != NULL && s2 != NULL)
+		len = (strlen(s1) + strlen(s2)) + 1;
+	/* Above: in case s1 or s2 have null value */
+	conc = malloc(len);
 	if (conc == NULL)
 		return (NULL);
+	/* Above: checking string with malloc */
 	for (p1 = 0; s1[p1]; p1++)
 		conc[p1] = s1[p1];
 	for (p2 = 0; s2[p2]; p2++, p1++)
