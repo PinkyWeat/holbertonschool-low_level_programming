5e89822bdaec93f3123d260e83d381f11332426d
Denisse Landau <denisselandau97@gmail.com>
2022-06-24 06:46:27 -0700

    cases fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -27,12 +27,18 @@ char *str_concat(char *s1, char *s2)
 	if (s1 != NULL)
 	{
 		for (p1 = 0; s1[p1]; p1++)
-			conc[p1] = s1[p1];
+		{
+			conc[p2] = s1[p1];
+			p2++;
+		}
 	}
 	if (s2 != NULL)
 	{	
-		for (p2 = 0; s2[p2]; p2++, p1++)
-			conc[p1] = s2[p2];
+		for (p1 = 0; s2[p1]; p1++)
+		{
+			conc[p2] = s2[p1];
+			p2++;
+		}
 	}
 	conc[p1] = '\0';
 	return (conc);
