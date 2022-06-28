8ab78be75f2196e04c93397f8799372f62ba3d8b
Denisse Landau <denisselandau97@gmail.com>
2022-06-28 13:01:55 -0700

    new dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include "dog_h"
+#include "dog.h"
 /**
  * init_dog - initialize a var type struct dog.
  * @d: dog.
@@ -8,8 +8,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog d;
-	dog.name = name;
-	dog.age = age;
-	dog.owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,5 +11,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
