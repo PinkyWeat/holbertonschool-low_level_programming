943ff1a5f62f9918f8c60659bdc2f6cb88ab30f6
Denisse Landau <denisselandau97@gmail.com>
2022-06-28 12:18:15 -0700

    first structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure.
+ * @age: age of dog.
+ * @name: name of dog.
+ * @owner: owner of dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
