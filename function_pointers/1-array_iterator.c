adfb36e24dee2964ef274299ee030f8e21e0e19b
Denisse Landau <denisselandau97@gmail.com>
2022-06-30 09:05:30 -0700

    function + array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - exec func given on each array elem.
+ * @array: the array.
+ * @size: size of array.
+ * @action: function.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}	
+}
