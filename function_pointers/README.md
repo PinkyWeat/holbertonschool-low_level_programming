9a4385bddd966ad415f7b9b1328ddc17b2eb56b7
Denisse Landau <denisselandau97@gmail.com>
2022-07-13 11:09:21 -0700

    readme

--- function_pointers/README.md
+++ function_pointers/README.md
@@ -16,3 +16,21 @@
 
       2-int-index.c - function that searches
 		      for an integer.
+
+		Calculator files:
+
+   	   3-main.c - entry point.
+
+   3-op-functions.c - File contains 5 functions
+		       (op_add, op_sub, op_mul,
+			 op_div and op_mod).
+
+    3-get_op_func.c - Contains function that
+		      selects the correct function
+		      to perform the operation
+	  	      asked by the user.
+
+	   3-calc.h - Contains all the function
+		      prototypes and data structs.
+
+(>^o^)> (>^o^)> (>^o^)> <(o-o)> <(^o^<) <(^o^<) <(^o^<)
--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -22,27 +22,10 @@ void print_times_table(int n)
 		{
 			k = i * j;
 
-			if (k > 99)
-			{
-				_putchar((k / 100) + 48);
-				_putchar(((k / 10) % 10) + 48);
-				_putchar((k % 10) + 48);
-			}
-			else if (k > 9)
-			{
-				_putchar((k / 10) + 48);
-				_putchar((k % 10) + 48);
-			}
-
-			else if( k <= 9)
-			{
-				_putchar((k % 10) + 48);
-			}
-
 			if (j <= n)
 			{
-				if(j < n)
-				{ 
+				if (j < n)
+				{
 					_putchar(',');
 				}
 				if (k < 99 && k > 9)
@@ -56,6 +39,24 @@ void print_times_table(int n)
 				}
 				_putchar(32);
 			}
+
+			if (k > 99)
+			{
+				_putchar((k / 100) + 48);
+				_putchar(((k / 10) % 10) + 48);
+				_putchar((k % 10) + 48);
+			}
+			else if (k > 9)
+			{
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+			}
+
+			else if (k <= 9)
+			{
+				_putchar((k % 10) + 48);
+			}
+
 		}
 		_putchar('\n');
 	}
