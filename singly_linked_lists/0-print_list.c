be7b24136226329940cfd82fefdf7c8b2b8ece60
Denisse Landau <denisselandau97@gmail.com>
2022-07-13 13:09:51 -0700

    cut lines

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,13 +11,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%i] %s\n", h->len, h->str);
-		}
 		counter++;
 		h = h->next;
 	}
