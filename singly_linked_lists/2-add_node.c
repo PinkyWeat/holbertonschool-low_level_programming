320caba4c196a527250d5f0320246aec227a36e2
Denisse Landau <denisselandau97@gmail.com>
2022-07-13 18:47:45 -0700

    fixes

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning.
+ * @head: first node.
+ * @str: what will be added.
+ * Return: address of new element or NULL.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	newNode = malloc(sizeof(list_t));
+	
+	/* checking malloc */
+	if (newNode == NULL)
+		return (NULL);
+	else /* creating new node */
+	{
+		newNode->str= strdup(str);
+		newNode->len = strlen(str); /* links data part */
+		newNode->next = *head; /* links address */
+	}
+	return (newNode->next);
+	
+}
