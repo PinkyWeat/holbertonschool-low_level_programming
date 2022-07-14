968529aeba1a42c7a1c54b09abd30e7e5a62102c
Denisse Landau <denisselandau97@gmail.com>
2022-07-13 21:08:44 -0700

    betty fixes

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- * first - prints before main.
+ * myConstructor - prints before main starts.
  */
-void myConstructor (void) 
+void myConstructor(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my hou    se upon my back!\n");
 }
