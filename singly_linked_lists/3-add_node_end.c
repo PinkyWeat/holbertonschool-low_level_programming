56c1add829587a14ae66a8c502f4955e43fce5d2
Denisse Landau <denisselandau97@gmail.com>
2022-07-13 20:16:28 -0700

    fixing seg fault

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,20 +9,24 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode, *aux;
 
-	if (head != NULL)
+	if (*head)
 		aux = *head;
 	newNode = malloc(sizeof(list_t));
 	if (!newNode)
 		return (NULL);
 	/* loop to give address of last node by not having next node*/
-	while (aux->next)
+	while (aux->next != NULL)
+	{
+		printf("antes");
 		aux = aux->next;
+		printf("despues");
+	}
 	/* make newNode last node */
 	aux->next = newNode;
 	/* Fill in new node */
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
-	newNode->next = *head;
+	newNode->next = NULL;
 	*head = newNode;
 	return (newNode);
 }
