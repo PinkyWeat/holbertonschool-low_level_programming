b874dbf397d71e5afb5bef7f285c09bb2707067a
Denisse Landau <denisselandau97@gmail.com>
2022-07-15 12:52:47 -0700

    fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (counter = 0; counter < (idx - 1) && aux; counter++)
 	{
 		if (*head == NULL)
-			*head = newNode;
+			return (NULL);
 		aux = aux->next;
 	}
 	/* Give address */
