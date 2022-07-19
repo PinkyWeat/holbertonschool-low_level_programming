82c6f0bb3e689900ac8fae046c7f7466a15538e7
Denisse Landau <denisselandau97@gmail.com>
2022-07-19 15:33:36 -0700

    fix it

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -10,8 +10,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (n)
 	{
 		if (index <= 64)
+		{
 			*n &= ~(1 << index);
-		return (1);
+			return (1);
+		}
 	}
 	else 
 		return (-1);
