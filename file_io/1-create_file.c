7a85d0e15e6ccccc601b160326371991d2e4fe5b
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 08:29:27 -0700

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: name of file.
+ * @text_contesnt: content.
+ * Return: 1 success, -1 failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int createMe, sizeMe = strlen(text_content);
+	mode_t modsie = S_IRUSR | S_IWUSR;
+
+	if (!filename)
+		return (-1);
+	else
+	{
+		createMe = open(filename, O_CREAT, modsie);
+		if (!text_content)
+			write(createMe, "", 0);
+		else
+			write(createMe, text_content, sizeMe);
+		close(createMe);
+		return (1);
+	}
+}
--- file_io/main.h
+++ file_io/main.h
@@ -4,9 +4,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
