a225b5aac7a71a7259cae1c5f07c64ba320b7f27
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 16:06:09 -0700

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int ac, char *av[])
 	/* Check if file_from exists */
 	if (file_from == - 1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
