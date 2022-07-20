d13b4ee203462f28a9b2724a127fadea6d610e3c
Denisse Landau <denisselandau97@gmail.com>
2022-07-19 17:43:37 -0700

    endianness

--- bit_manipulation/100-get_endianness.c
+++ bit_manipulation/100-get_endianness.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * get_endianness - checks the endianness.
+ * Return: if lil or big.
  */
 int get_endianness(void)
 {
 	unsigned int x = 1;
-	char *c = (char*) &x;
+	char *c = (char *) &x;
 
-	return (int)*c;
+	return ((int)*c);
 }
