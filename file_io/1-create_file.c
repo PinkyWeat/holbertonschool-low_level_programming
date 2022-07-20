174240a385ac1c2df33362af2d3fdb2ff46b1770
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 08:59:06 -0700

    hottest fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,6 +20,8 @@ int create_file(const char *filename, char *text_content)
 	/* if there's content write it */
 	if (text_content)
 		write(createMe, text_content, sizeMe);
+	else
+		return (1);
 	close(createMe);
 	return (1);
 }
