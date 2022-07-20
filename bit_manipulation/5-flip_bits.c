0bc012d73fd8805480823dd3cd6760088c703002
Denisse Landau <denisselandau97@gmail.com>
2022-07-19 17:11:25 -0700

    fix

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int bits = 0, i = 0;
 
-	while (i != 63)
+	while (i != 64)
 	{
 		if ((n & 1) != (m & 1))
 			bits++;
