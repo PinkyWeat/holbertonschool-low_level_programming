a65ca500df49400f9d76f54145cb17da9383fec3
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 10:56:11 -0700

    wronly

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the file.
+ * @text_content: files content.
+ * Return 1 on success or -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int createMe;
+
+	if (!filename)
+		return(-1);
+	/* create file */
+       createMe = open(filename, O_WRONLY | O_APPEND);
+        /* check creation */
+        if (createMe == -1)
+                return (-1);
+        /* if there's content write it */
+        if (text_content)
+                write(createMe, text_content, strlen(text_content));
+        close(createMe);
+        return (1);}
