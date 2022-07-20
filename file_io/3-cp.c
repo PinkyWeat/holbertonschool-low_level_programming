c12649d74aad6e46cbc9e0d333c740a5d94c1ef5
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 15:59:16 -0700

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -45,7 +45,7 @@ int main(int ac, char *av[])
 		}
 		/* write? */
 		writeMe = write(file_to, cCount, readMe);
-		if (readMe == -1)
+		if (writeMe == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
