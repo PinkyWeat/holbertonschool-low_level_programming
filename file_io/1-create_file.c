6c8d1e642a989f51153a31802ecdf03e7d24fb5c
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 10:01:08 -0700

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,8 +20,6 @@ int create_file(const char *filename, char *text_content)
 	/* if there's content write it */
 	if (text_content)
 		write(createMe, text_content, sizeMe);
-	else
-		return (1);
 	close(createMe);
 	return (1);
 }
