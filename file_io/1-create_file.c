d0882d190c53007e4108178dd41100318b5d6519
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 10:07:55 -0700

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,24 +7,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int createMe, sizeMe = strlen(text_content), newW;
-	mode_t modsie = S_IRUSR | S_IWUSR;
+	int createMe, sizeMe = strlen(text_content);
 
 	if (!filename)
 		return (-1);
 	/* create file */
-	createMe = open(filename, O_RDWR | O_CREAT | O_TRUNC, modsie);
+	createMe = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	/* check creation */
 	if (createMe == -1)
 		return (-1);
 	/* if there's content write it */
 	if (text_content)
-		newW = write(createMe, text_content, sizeMe);
-	if (newW == -1)
-	{
-		close(createMe);
-		return (-1);
-	}
+		write(createMe, text_content, sizeMe);
 	close(createMe);
 	return (1);
 }
