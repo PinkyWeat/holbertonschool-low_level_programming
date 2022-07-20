eabd25b2683addd5ebd492abea81296f4f3f4794
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 16:13:20 -0700

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,7 +40,7 @@ int main(int ac, char *av[])
 		readMe = read(file_from, cCount, 1024);
 		if (readMe == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		/* write? */
