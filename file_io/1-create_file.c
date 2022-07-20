88c0f872c4a293dde03011e9b6147b05fc576297
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 08:55:47 -0700

    hot fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,19 +7,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int createMe, sizeMe = strlen(text_content);
+	int createMe;
 	mode_t modsie = S_IRUSR | S_IWUSR;
 
 	if (!filename)
-	{
-		createMe = open(filename, O_CREAT, modsie);
-		if (!text_content)
-			write(createMe, "1", 1);
-		else
-			write(createMe, text_content, sizeMe);
-		close(createMe);
-	}
-	if (filename == NULL)
+		return(-1);
+	/* create file */
+	createMe = open(filename, O_RDWR | O_CREAT | O_TRUNC, modsie);
+	/* check creation */
+	if (createMe == -1)
 		return (-1);
+	/* if there's content write it */
+	if (text_content)
+		write(createMe, "1", 1);
+	close(createMe);
 	return (1);
 }
