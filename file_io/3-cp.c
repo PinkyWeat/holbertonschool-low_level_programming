18c3a85cb189dd068caf197921b65eceab7c8290
Denisse Landau <denisselandau97@gmail.com>
2022-07-20 17:45:12 -0700

    davai

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,34 +30,19 @@ void copy_text(const char *from, const char *to)
 	file_from = open(from, O_RDONLY);
 	/* Check if file_from exists */
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
 
 	file_to = open(to, O_CREAT | O_TRUNC | O_WRONLY, modsie);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to),exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to), exit(99);
 	readMe = read(file_from, cCount, 5555);
 	if (readMe == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", to);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", to), exit(98);
 	writeMe = write(file_to, cCount, readMe);
 	if (writeMe == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to), exit(99);
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 }
