ace6d607a8c2df57395508b3db4f2315d014c712
Denisse Landau <denisselandau97@gmail.com>
2022-07-22 18:12:31 -0700

    sum

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum of all the data (n) of a dlistint_t linked list.
+ * @head: pointer to first node.
+ * Return sum of all data.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int res = 0;
+
+	while (head)
+	{
+		res += head->n;
+		head = head->next;
+	}
+	return (res);
+}
