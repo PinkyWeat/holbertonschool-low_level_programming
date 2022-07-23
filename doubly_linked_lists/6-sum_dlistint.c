014126cd717accd3da20253b4a15ba06138eddf0
Denisse Landau <denisselandau97@gmail.com>
2022-07-22 18:13:26 -0700

    sum

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,7 +2,7 @@
 /**
  * sum_dlistint - sum of all the data (n) of a dlistint_t linked list.
  * @head: pointer to first node.
- * Return sum of all data.
+ * Return: sum of all data.
  */
 int sum_dlistint(dlistint_t *head)
 {
