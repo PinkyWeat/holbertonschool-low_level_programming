f2f3631c3adc708351e496bb71df971fb4611000
Denisse Landau <denisselandau97@gmail.com>
2022-08-18 12:23:16 -0700

    fix variable type

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newTable = malloc(sizeof(hash_table_t));
-	int i = 0;
+	unsigned long int i = 0;
 
 	if (!newTable)
 		return (NULL);
