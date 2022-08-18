84f26a3717cfe4d16eb25dde9b7876003260667c
Denisse Landau <denisselandau97@gmail.com>
2022-08-18 12:26:18 -0700

    size fix in malloc hash_node_t

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!newTable)
 		return (NULL);
 	newTable->size = size;
-	newTable->array = malloc(sizeof(hash_node_t) * size);
+	newTable->array = malloc(sizeof(hash_node_t *) * size);
 	if (!newTable->array)
 	{
 		free(newTable);
