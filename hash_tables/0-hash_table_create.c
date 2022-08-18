9a82881edc033dd8538534a437cd93f1b7317544
Denisse Landau <denisselandau97@gmail.com>
2022-08-18 12:10:59 -0700

    upd new Table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hast_table_create - creates hash tables.
+ * hash_table_create - creates hash tables.
  * @size: size of the array.
  * Return: a pointer to a newly created hash table.
  */
