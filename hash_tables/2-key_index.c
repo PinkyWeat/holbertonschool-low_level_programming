73d8806f2275bd345c6b636a1f9d4e455eb89f68
Denisse Landau <denisselandau97@gmail.com>
2022-08-20 11:24:02 -0700

    task 2 + ups

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,45 +1,11 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - adds an element to the hash table.
- * @ht: hast table we are adding/updating the key/value to.
- * @key: the key.
- * @value: the value.
- * Return: 1 on success, 0 otherwise.
+ * key_index - gives you the index of a key.
+ * @key: key to find the index.
+ * @size: size of the array of the hash table.
+ * Return: index which key/value should be stored in array hash table.
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	char *aux = NULL;
-	hash_node_t *newNode = malloc(sizeof(hash_node_t));
-
-	if (!ht) /* Check if hash table */
-	{
-		free(newNode);
-		return (0);
-	}
-	if (!newNode) /* Check malloc */
-		return (0);
-	if (!key) /* Key cannot be empty */
-		return (0);
-	newNode->key = key; /* sino se le agrega entonces */
-	if (!ht->array) /* Check if hay algo en el lugar de array*/
-	{
-		free(ht->array); /* y lo libera */
-		ht->array = NULL;
-	}
-	if (value) /* Check value before adding it */
-		aux = strdup(value); /* something so dup */
-	else
-	{
-		free(aux);
-		aux = NULL; /* empty string*/
-	}
-	newNode->value = aux; /* le puse value */
-	if (ht->array)/* ver sobre el next de newNode comoo NULL ponele*/
-	
-
-	if (key == )
-	{ /* si no tiene value hay que agregar */
-		newNode->value = value;
-		newNode->key = 
-	}
+	return((hash_djb2(key) % size));
 }
