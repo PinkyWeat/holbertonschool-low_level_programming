ab7c7deb0250aa789f0c0e618676dc35340fbb12
Denisse Landau <denisselandau97@gmail.com>
2022-08-21 14:46:44 -0700

    betty fix

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 /**
- *
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hast table we are looking into.
+ * @key: what we're looking for.
+ * Return: value associated with the element, or NULL.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
