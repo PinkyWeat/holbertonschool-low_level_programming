0693955bc8780f9e178b148815a64d6dc49da49e
Denisse Landau <denisselandau97@gmail.com>
2022-08-21 15:01:05 -0700

    try1

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_talbe_print - prints a hash table.
+ * @ht: the hash table we'll print.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int kindex = 0;
+	hash_node_t *aux = NULL;
+	char centinel = '\0';
+
+	if (!ht)
+		return;
+	printf("{");
+	while (kindex < ht->size) /* until kindex reaches size */
+	{
+		aux = ht->array[kindex]; /* Open Linked List*/
+		while (aux)
+		{
+			if (centinel != '\0')
+				printf(", ");
+			printf("'%s': '%s'", aux->key, aux->value);
+			if (centinel == '\0')
+				centinel = '1';
+			aux = aux->next; 
+		}
+		kindex++;
+	}
+	printf("}\n");
+}
