a591f617f35d66e094f67ec8e23bf1c5b3b18743
Denisse Landau <denisselandau97@gmail.com>
2022-08-29 16:38:32 -0700

    quick printfs fixes

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,17 +4,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int counter = 0, index = 0;
+	int index = 0;
 	size_t i;
 
 	if (!array)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (array[i] == value && counter < 1)
-		{	
-			counter++;
+		if (array[i] == value)
+		{
 			index = i;
+			break;
 		}
 		printf("Value checked array[%ld] = [%i]\n", i, array[i]);
 	}
